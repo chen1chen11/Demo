adc6090969251435e8feda524db256714b884049
Yuchen Guo <eliza_guo@outlook.com>
2026-03-18 15:24:21 +0800

    add GA4 SDK

--- src/components/ProjectWorkbench.vue
+++ src/components/ProjectWorkbench.vue
@@ -93,15 +93,7 @@
   </div>
 </template>
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5F34DZEVC5"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-5F34DZEVC5');
-</script>
 <script>
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 
